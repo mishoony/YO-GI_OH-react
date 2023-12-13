c8b13e423313d1d4e1e11f019b4e795de84b853e
mishoony <75951465+mishoony@users.noreply.github.com>
2023-12-13 12:13:12 +0200

    initialize react in the project

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Products from "./components/Products/Products"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Products/>
+    <Home/>
+    </>
+    
+  )
+}
+
+export default App
